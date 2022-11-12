875d42a6032b1de2e19866dfff3577fecbe9999f
Atul singh bhandari <atulbhan40@gmail.com>
2022-11-12 10:11:38 +0530

    created singleton class

---
+++ ProgramMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class ProgramMain
+    {
+
+
+        public static void ObjectMethod1()
+        {
+            SingletonClass obj = SingletonClass.getInstance();
+
+        }
+        public static void ObjectMethod2()
+        {
+            SingletonClass obj1 = SingletonClass.getInstance();
+        }
+        static void Main(string[] args)
+        {
+
+            Parallel.Invoke(
+            () => { ObjectMethod1(); },
+            () => { ObjectMethod2(); }
+                     );
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SingletonClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public  sealed class SingletonClass
+    {
+        private static SingletonClass instance = null;
+        private static int counter = 0;
+        private static readonly object objlock = new object();
+
+        private SingletonClass()
+        {
+            counter++;
+            Console.WriteLine(counter.ToString()+ " " + "hello i am private constructor from singleton class");
+        }
+        public static SingletonClass getInstance()
+        {
+            lock (objlock)
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonClass();
+                }
+            }
+            return instance;
+        }   
+    }
+}
+
+
+
